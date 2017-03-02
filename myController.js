b42cb864cd1e1ce637e14dc2f33cc81a24cecd18
Caroline Moers <Caroline_Moers@gap.com>
2017-03-02 19:20:44 -0300

    Create table with sorting

--- myController.js
+++ myController.js
@@ -2,4 +2,20 @@ app.controller("myController",
   function($scope) {
     $scope.name = "Carol";
     $scope.names = ["Carol", "Desi", "Luiz", "Marcelo"];
+
+    $scope.people = [
+        {name:'Jani',country:'Norway'},
+        {name:'Carl',country:'Sweden'},
+        {name:'Margareth',country:'England'},
+        {name:'Hege',country:'Norway'},
+        {name:'Joe',country:'Denmark'},
+        {name:'Gustav',country:'Sweden'},
+        {name:'Birgit',country:'Denmark'},
+        {name:'Mary',country:'England'},
+        {name:'Kai',country:'Norway'}
+    ];
+
+    $scope.orderByMe = function(x) {
+      $scope.myOrderBy = x;
+    }
 });
